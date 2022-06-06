f28162736ece0c84cd4c551e896b6c97cd2a8ff4
songa1 <songachillethe1her@gmail.com>
2022-06-06 09:36:54 +0200

    I have added working database connection

--- src/main/java/com/store/awtapp/AWTAPP.java
+++ src/main/java/com/store/awtapp/AWTAPP.java
@@ -5,22 +5,30 @@
 
 package com.store.awtapp;
 
+import java.sql.*;
+
 /**
  *
  * @author CISHAHAYO
  */
 public class AWTAPP {
-    public String driver = "com.mysql.cj.jdbc.Driver";
-    public String url = "jdbc:mysl://localhost:3000/store";
     
     public static void main(String[] args) {
         System.out.println("Hello, Let's open the App!");
         new Login().setVisible(true);
         
         try{
-            Class.forName(driver);
-        }catch(ClassNotFoundException e){
-            System.out.println("Driver not found!");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysl://localhost:3306/store", "root", "");
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery( "select * from users" );
+            
+            while(rs.next()){
+                System.out.println();
+            }
+            conn.close();
+        }catch(Exception e){
+            System.out.println(e);
         }
     }
 }
